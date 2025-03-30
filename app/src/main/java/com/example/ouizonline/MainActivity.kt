6d24f0e0d28c3494ad8f20c09e569b32d656ede2
Ak-2003-ai <sreeakshay10@gmail.com>
2025-03-30 11:58:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ouizonline/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.ouizonline
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ouizonline.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var quizModelList : MutableList<QuizModel>
+    lateinit var adapter: QuizListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        quizModelList = mutableListOf()
+        getDataFromFirebase()
+
+    }
+
+    private fun setupRecyclerView(){
+        binding.progressBar.visibility = View.GONE
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+    }
+
+    private fun getDataFromFirebase(){
+        binding.progressBar.visibility = View.VISIBLE
+        FirebaseDatabase.getInstance().reference
+            .get()
+            .addOnSuccessListener { dataSnapshot->
+                if(dataSnapshot.exists()){
+                    for(snapshot in dataSnapshot.children){
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            quizModelList.add(quizModel)
+                        }
+                    }
+                }
+                setupRecyclerView()
+            }
+    }
+}
